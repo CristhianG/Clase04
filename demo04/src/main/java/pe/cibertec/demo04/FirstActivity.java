06a143b9cfa9c4efd73d6417f62435733bbdeb0f
Luis Alonso Rios <pxlrios@cibertec.edu.pe>
2016-04-30 17:18:40 -0500

    Demo 04

---
+++ demo04/src/main/java/pe/cibertec/demo04/FirstActivity.java
@@ -0,0 +1,77 @@
+package pe.cibertec.demo04;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+import pe.cibertec.demo04.adapter.listview.LVFirstAdapter;
+import pe.cibertec.demo04.entities.Persona;
+
+public class FirstActivity extends AppCompatActivity {
+
+    private ListView lvFirstPerson;
+    private LVFirstAdapter mLVFirstAdapter;
+    private Button btFirstAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_activity);
+
+        lvFirstPerson = (ListView) findViewById(R.id.lvFirstPerson);
+        btFirstAdd = (Button) findViewById(R.id.btFirstAdd);
+
+        mLVFirstAdapter = new LVFirstAdapter(FirstActivity.this);
+
+        lvFirstPerson.setAdapter(mLVFirstAdapter);
+
+        btFirstAdd.setOnClickListener(btFirstAddOnClickListener);
+        lvFirstPerson.setOnItemClickListener(lvFirstPersonOnItemClickListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 99)
+                mLVFirstAdapter.add(data.<Persona>getParcelableExtra("arg_persona"));
+            else {
+                Persona persona = data.getParcelableExtra("arg_persona");
+                for (int i = 0; i < mLVFirstAdapter.getCount(); i++) {
+                    Persona personaOld = mLVFirstAdapter.getItem(i);
+                    if (persona.getId() == personaOld.getId()) {
+                        personaOld.setName(persona.getName());
+                        personaOld.setLast(persona.getLast());
+                        personaOld.setAge(persona.getAge());
+                        personaOld.setDoc(persona.getDoc());
+                        personaOld.setPhone(persona.getPhone());
+
+                        mLVFirstAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        }
+    }
+
+    View.OnClickListener btFirstAddOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+            intent.putExtra("arg_list_size", mLVFirstAdapter.getCount() + 1);
+            startActivityForResult(intent, 99);
+        }
+    };
+
+    AdapterView.OnItemClickListener lvFirstPersonOnItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Persona persona = (Persona) parent.getItemAtPosition(position);
+            Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+            intent.putExtra("arg_persona", persona);
+            startActivityForResult(intent, 98);
+        }
+    };
+}
---
+++ demo04/src/main/java/pe/cibertec/demo04/entities/Persona.java
@@ -0,0 +1,112 @@
+package pe.cibertec.demo04.entities;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Android-SAB-PM on 30/04/2016.
+ */
+public class Persona implements Parcelable {
+    private int id;
+    private String name;
+    private String last;
+    private String phone;
+    private String doc;
+    private int age;
+
+//    public Persona(int id, String name, String last, String phone, String doc, int age) {
+//        this.id = id;
+//        this.name = name;
+//        this.last = last;
+//        this.phone = phone;
+//        this.doc = doc;
+//        this.age = age;
+//    }
+
+    public Persona() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDoc() {
+        return doc;
+    }
+
+    public void setDoc(String doc) {
+        this.doc = doc;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.name);
+        dest.writeString(this.last);
+        dest.writeString(this.phone);
+        dest.writeString(this.doc);
+        dest.writeInt(this.age);
+    }
+
+    protected Persona(Parcel in) {
+        this.id = in.readInt();
+        this.name = in.readString();
+        this.last = in.readString();
+        this.phone = in.readString();
+        this.doc = in.readString();
+        this.age = in.readInt();
+    }
+
+    public static final Creator<Persona> CREATOR = new Creator<Persona>() {
+        @Override
+        public Persona createFromParcel(Parcel source) {
+            return new Persona(source);
+        }
+
+        @Override
+        public Persona[] newArray(int size) {
+            return new Persona[size];
+        }
+    };
+}
